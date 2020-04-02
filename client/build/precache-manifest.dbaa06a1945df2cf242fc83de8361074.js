a700e2875531b07cd5677a946b324076af8aeb28
pdhoward <patrick.howard@hotmail.com>
2020-04-02 14:46:56 -0400

    fix build file

---
+++ client/build/precache-manifest.dbaa06a1945df2cf242fc83de8361074.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad0605ab98a5ad21f4691f6d576a53c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f00176d0425b1ec23fa0",
+    "url": "/static/css/2.45c175d8.chunk.css"
+  },
+  {
+    "revision": "472dc901de0922b0f6c9",
+    "url": "/static/css/main.a68680de.chunk.css"
+  },
+  {
+    "revision": "f00176d0425b1ec23fa0",
+    "url": "/static/js/2.336b5c08.chunk.js"
+  },
+  {
+    "revision": "472dc901de0922b0f6c9",
+    "url": "/static/js/main.4634403d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "009911f990793dbdbaa835ff32b6aaae",
+    "url": "/static/media/vip.009911f9.png"
+  }
+]);
