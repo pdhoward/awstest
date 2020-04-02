0cf05103fb710f650b628166945921660431ec02
pdhoward <patrick.howard@hotmail.com>
2020-04-02 13:44:12 -0400

    updating for elastic beanstalk deploy

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const usersRoutes = require('./routes')
 
 const app = express()
 
-const MONGODB_URI = process.env.ATLAS_ACTIONS || 'mongodb://localhost/auth'
-const PORT = process.env.PORT || 8081
+const MONGODB_URI = process.env.ATLAS_AUTH || 'mongodb://localhost/auth'
+const PORT = process.env.PORT || 4000
 
 mongoose.set('useCreateIndex', true)
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, (err) => {
